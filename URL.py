bc83d2887f52095eaa1626d735f7bdc5b7febd21
ardsew <andrewkim260@gmail.com>
2024-10-21 10:20:58 -0700

    feat: font layout changes

--- URL.py
+++ URL.py
@@ -1,7 +1,14 @@
 import socket
 import ssl
 import tkinter
+import tkinter.font
 from typing import List
+
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+FONTS = {}  # caching purposes
 
 
 class URL:
@@ -65,12 +72,9 @@ class URL:
         s.close()
 
         print('Response Headers: \n', response_headers)
+        # print(body)
         return body
 
-
-WIDTH, HEIGHT = 800, 600
-HSTEP, VSTEP = 13, 18
-SCROLL_STEP = 100
 
 class Browser:
     def __init__(self):
@@ -90,44 +94,135 @@ class Browser:
 
     def load(self, url: URL):
         body = url.request()
-        text = lex(body)
-        self.display_list = layout(text)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
         self.draw()
     
     def draw(self):
         self.canvas.delete('all')
-        for x, y, c in self.display_list:
+        for x, y, c, font in self.display_list:
             if y > self.scroll + HEIGHT:
                 continue
             if y + VSTEP < self.scroll:
                 continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=c, anchor='nw', font=font)
 
 
-def lex(body: str):
+class Text:
+    def __init__(self, text: str):
+        self.text = text
+
+    def __str__(self):
+        return f"Text object with value: {self.text}"
+
+
+class Tag:
+    def __init__(self, tag: str):
+        self.tag = tag
+
+    def __str__(self):
+        return f"Tag object with value: {self.tag}"
+
+
+class Layout:
+    def __init__(self, tokens: List[Tag] | List[Text]):
+        self.display_list = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.line = []
+        self.size = 12  # font size
+
+        for tok in tokens:
+            self.token(tok)
+
+        self.flush()
+
+    def token(self, tok: Tag | Text):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            print('self.weight has changed to BOLD!!')
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+    
+    def word(self, word: Text):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(' ')
+    
+    def flush(self):
+        if self.line == []:
+            return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        max_descent = max([metric["descent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = []
+
+
+def lex(body: str) -> List[Tag] | List[Text]:
+    out = []
+    buffer = ""
     in_tag = False
-    text = ""
     for c in body:
         if c == '<':
             in_tag = True
+            if buffer:
+                out.append(Text(buffer))
+            buffer = ""
         elif c == '>':
             in_tag = False
-        elif not in_tag:
-            # print(c, end='')
-            text += c
-    return text
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+    return out
 
 
-def layout(text: str) -> List[str]:
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP
-            cursor_x = HSTEP
-    return display_list
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(
+            size=size, 
+            weight=weight,
+            slant=style
+        )
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
 
 
 if __name__ == '__main__':
